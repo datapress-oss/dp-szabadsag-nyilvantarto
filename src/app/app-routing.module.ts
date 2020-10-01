4111d466346b4653fa2e249de37fc5ec396cfe05
Ferenc Ónodi <onodiferenc03@gmail.com>
2020-10-01 19:27:50 +0200

    fix: fix app-routing module.ts routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,10 @@ import { Routes, RouterModule } from '@angular/router';
 import {TestComponenetComponent} from './test-componenet/test-componenet.component'
 
 const routes: Routes = [
- 
+  {
+    path: 'test',
+    component: TestComponenetComponent,
+  },
 ];
 
 @NgModule({
