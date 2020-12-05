c21de53968f5168c0ab776bdcae4c4eb30306e99
Balazs Nyirfa <balazsnyirfa@gmail.com>
2020-12-05 11:06:26 +0100

    refactor: move setUser logic into navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from './../auth.service';
+import { LoggedInUser, role } from './../classes/user';
 
 @Component({
   selector: 'app-navbar',
@@ -7,13 +8,37 @@ import { AuthService } from './../auth.service';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
+  isLoggedIn: boolean;
+  loggedInUser: LoggedInUser;
 
-  isLoggedIn(): boolean {
-    return this.auth.isLoggedIn();
+  private setUser(): void {
+    this.isLoggedIn = true;
+    // get latest user obj from localStorage
+    const user: LoggedInUser = JSON.parse(localStorage.getItem('loggedInUser'));
+    this.loggedInUser = user;
+  }
+
+  public logOutEventHandler(): void {
+    // clean user values
+    this.isLoggedIn = false;
+    this.loggedInUser.username = null;
+    this.loggedInUser.roles = [];
   }
 
   constructor(private auth: AuthService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    if (this.auth.isLoggedIn()) {
+      // set user if already logged in
+      this.setUser();
+    }
+    this.auth.userLogInEventListener().subscribe(userLogIn => {
+      // skip userLogIn with value of null
+      if (userLogIn !== null) {
+        // set user on every new log in event
+        this.setUser();
+      }
+    });
+  }
 
 }
--- src/app/navbar/user-menu/user-menu.component.ts
+++ src/app/navbar/user-menu/user-menu.component.ts
@@ -1,7 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { faUser } from '@fortawesome/free-solid-svg-icons';
 import { AuthService } from './../../auth.service';
-import { LoggedInUser } from './../../classes/user';
 
 @Component({
   selector: 'app-user-menu',
@@ -9,35 +8,16 @@ import { LoggedInUser } from './../../classes/user';
   styleUrls: ['./user-menu.component.scss']
 })
 export class UserMenuComponent implements OnInit {
+  @Input() isLoggedIn: boolean;
+  @Output() logOutEvent: EventEmitter<boolean> = new EventEmitter<boolean>();
   faUser = faUser;
-  isLoggedIn: boolean;
-  username: string;
 
   onLogOut(): void {
-    this.isLoggedIn = false;
+    this.logOutEvent.emit(false);
     this.auth.logOut();
-  }
-
-  private setUser(): void {
-    this.isLoggedIn = true;
-    // get latest user obj from localStorage
-    const user: LoggedInUser  = JSON.parse(localStorage.getItem('loggedInUser'));
-    this.username = user.username;
   }
 
   constructor(private auth: AuthService) {}
 
-  ngOnInit(): void {
-    if (this.auth.isLoggedIn()) {
-      // set user if already logged in
-      this.setUser();
-    }
-    this.auth.userLogInEventListener().subscribe(userLogIn => {
-      // skip userLogIn with value of null
-      if (userLogIn !== null) {
-        // set user on every new log in event
-        this.setUser();
-      }
-    });
-  }
+  ngOnInit(): void {}
 }
