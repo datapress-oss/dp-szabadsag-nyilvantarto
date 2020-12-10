6e1c9ca0d9f3fead925a62bb8495cacd8b0405c5
Balazs Nyirfa <balazsnyirfa@gmail.com>
2020-12-11 00:11:28 +0100

    feat: add calendar component to user view

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,14 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { DateManagerService } from './../date-manager.service';
+import * as moment from 'moment';
+import { Year } from './../classes/calendarClasses';
 
 @Component({
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.scss']
 })
 export class UserComponent implements OnInit {
+  currentYearCalendar: Year;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private dateManager: DateManagerService) {
+    this.currentYearCalendar = this.dateManager.createCalendar(moment().year());
   }
+
+  ngOnInit(): void {}
 
 }
