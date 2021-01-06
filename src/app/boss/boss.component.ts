4de29ba05e0814fbfdc2038f0c8b19608f9c7433
Balazs Nyirfa <balazsnyirfa@gmail.com>
2021-01-06 14:37:06 +0100

    feat: integrate date-proposal into boss component

--- src/app/boss/boss.component.ts
+++ src/app/boss/boss.component.ts
@@ -1,10 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { mockHolidays, Holiday } from './../classes/holiday';
 
 @Component({
   templateUrl: './boss.component.html',
   styleUrls: ['./boss.component.scss']
 })
 export class BossComponent implements OnInit {
+  mockHolidays = mockHolidays;
+
+  bossAcceptEventHandler(): void {
+    console.log('boss accept event');
+  }
+  bossCancelEventHandler(): void {
+    console.log('boss cancel event');
+  }
 
   constructor() { }
 
